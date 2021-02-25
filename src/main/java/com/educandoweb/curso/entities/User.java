e707e3ad7a86e200b18c32dc259b56c5bc667ba0
EVERTON DANTAS <evertonbonfim@yahoo.com.br>
2021-02-25 15:40:27 -0300

    Order, Instant Iso 8601 - PART 1

--- src/main/java/com/educandoweb/curso/entities/User.java
+++ src/main/java/com/educandoweb/curso/entities/User.java
@@ -1,13 +1,18 @@
 package com.educandoweb.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -19,6 +24,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List <Order> orders = new ArrayList<>();
+	
 	
 	public User (){
 		
@@ -70,8 +79,18 @@ public class User implements Serializable {
 	}
 
 	public void setPassword(String password) {
-		this.password = password;
+	this.password = password;
 	}
+	
+	public List <Order> getOrders() {
+		return orders;
+	}
+	
+	
+	
+	
+	
+	
 
 	@Override
 	public int hashCode() {
@@ -79,6 +98,10 @@ public class User implements Serializable {
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
+		
+		
+
+		
 	}
 
 	@Override
@@ -98,5 +121,7 @@ public class User implements Serializable {
 		return true;
 	}
 
-}
+	}
+
+
 
